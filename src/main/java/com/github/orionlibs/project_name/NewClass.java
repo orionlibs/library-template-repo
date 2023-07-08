5bf9414af63bcc74f8c69b90a0cc47c75273c3f3
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-08 20:39:03 +0100

    updated

--- src/main/java/com/github/orionlibs/project_name/NewClass.java
+++ src/main/java/com/github/orionlibs/project_name/NewClass.java
@@ -1,24 +1,27 @@
 package com.github.orionlibs.project_name;
 
+import com.github.orionlibs.project_name.config.ConfigurationService;
+import com.github.orionlibs.project_name.config.OrionConfiguration;
 import java.io.IOException;
 import java.util.logging.Handler;
-import java.util.logging.Logger;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
+@NoArgsConstructor
 public class NewClass
 {
     private final static Logger log;
-    //private final Environment springEnv;
-    //private final OrionConfiguration featureConfiguration;
+    private Environment springEnv;
+    private OrionConfiguration featureConfiguration;
 
     static
     {
         log = Logger.getLogger(NewClass.class.getName());
     }
-    
-    
+
     @Autowired
     public NewClass(final Environment springEnv) throws IOException
     {
@@ -27,13 +30,13 @@ public class NewClass
         loadLoggerConfiguration();
         ConfigurationService.registerConfiguration(featureConfiguration);
     }
-    
-    
+
+
     private void loadLoggerConfiguration() throws IOException
     {
         LogManager.getLogManager().readConfiguration(OrionConfiguration.loadLoggerConfigurationAndGet(springEnv).getAsInputStream());
     }
-    
+
 
     static void addLogHandler(Handler handler)
     {
