bf8d213a321e6a7125590b0ab18eb8ed10d9eac0
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-09 18:34:01 +0100

    updated

--- src/test/java/io/github/orionlibs/project_name/config/FakeTestingSpringConfiguration.java
+++ src/test/java/io/github/orionlibs/project_name/config/FakeTestingSpringConfiguration.java
@@ -1,5 +1,6 @@
 package io.github.orionlibs.project_name.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -8,6 +9,8 @@ public class FakeTestingSpringConfiguration
     @Configuration
     @Import(
                     {FakeSpringMVCConfiguration.class})
+    @ComponentScan(basePackages =
+                    {"io.github.orionlibs"})
     public static class FakeConfiguration
     {
     }
