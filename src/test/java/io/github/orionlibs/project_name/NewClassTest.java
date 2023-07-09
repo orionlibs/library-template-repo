1361d2702b37b1f4be1524fd783c3e4f73360ef5
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-09 22:22:22 +0100

    updated

--- src/test/java/io/github/orionlibs/project_name/NewClassTest.java
+++ src/test/java/io/github/orionlibs/project_name/NewClassTest.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.logging.LogManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
